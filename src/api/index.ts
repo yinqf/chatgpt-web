cd3d95b24aefc5822084ce5e08c1ad24166411ad
yinqf <yinqf7437@gmail.com>
2023-04-02 11:41:16 +0800

    对数据接口添加base64编码

--- src/api/index.ts
+++ src/api/index.ts
@@ -29,10 +29,10 @@ export function fetchChatAPIProcess<T = any>(
 ) {
   const settingStore = useSettingStore()
 
-	// base64编码
-	const encoder: TextEncoder = new TextEncoder()
-	const data: Uint8Array = encoder.encode(params.prompt)
-	params.prompt = btoa(String.fromCharCode(...data))
+  // base64编码
+  const encoder: TextEncoder = new TextEncoder()
+  const data: Uint8Array = encoder.encode(params.prompt)
+  params.prompt = btoa(String.fromCharCode(...data))
 
   return post<T>({
     url: '/chat-process',
